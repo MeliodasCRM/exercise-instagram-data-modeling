280478f5d4154681bac09043fbac3036676b3663
MeliodasCRM <Pabermb@gmail.com>
2024-11-29 19:33:34 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -1,32 +1,63 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    comments = relationship("Comment", back_populates="user")
+
+class Comment(Base):
+    __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    post_id = Column(Integer, ForeignKey("post.id"), nullable=False)
 
-    def to_dict(self):
-        return {}
+    user = relationship("User", back_populates="comments")
+    post = relationship("Post", back_populates="comments")
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    
+    comments = relationship("Comment", back_populates="post")
+
+class Type_Of_Media(enum.Enum):
+    Image = "image"
+    Video = "video"
+    Audio = "audio"
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column("type", Enum(Type_Of_Media))
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Follower(Base):
+    __tablename__ = 'follower'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    # id del que está siguiendo a otro, el seguidor
+    user_from_id = Column(Integer, ForeignKey('user.id'), nullable=False) 
+    # id del que es seguido
+    user_to_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+
+    follower = relationship("User", foreign_keys=[user_from_id])
+    followee = relationship("User", foreign_keys=[user_to_id])
 
 ## Draw from SQLAlchemy base
 try:
